677fa7578861a2d6a3d29e3b142492f055ff1503
John <john@taxtim.com>
2016-06-20 12:56:54 +0200

    Added Messenger Api getUser method, and refined send method. Abstracted Curl methods out into a lightweight Curl class.

--- src/JK/FacebookMessenger/Api/MessengerApi.php
+++ src/JK/FacebookMessenger/Api/MessengerApi.php
@@ -10,6 +10,7 @@ namespace JK\FacebookMessenger\Api;
 
 use JK\FacebookMessenger\Core\Entity\Recipient;
 use JK\FacebookMessenger\Core\Message;
+use JK\FacebookMessenger\Curl\Curl;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
@@ -36,54 +37,38 @@ class MessengerApi
         $this->token = $token;
     }
 
-    public function send(Recipient $recipient, Message $message, $path = '/me/messages')
+    public function sendMessage(Recipient $recipient, Message $message)
     {
 
+        // Init curl
+        $curl = new Curl();
+
         // Build the URL
-        $url = self::FB_API_URL . $path;
+        $url = self::FB_API_URL . '/me/messages';
 
         // Serialize the content
-        $content = $this->serializer->serialize(
-            [
+        $content = $this->serializer->serialize([
                 'recipient' => $recipient,
                 'message' => $message,
                 'access_token' => $this->token
-            ],
-            'json');
+            ], 'json');
 
-        // Initialize Curl and set options
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $content);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-
-        // Make the call
-        $response  = curl_exec($ch);
-
-        // Close the resource
-        curl_close($ch);
-
-        // Decode and return the JSON response
-        return json_decode($response, true);
+        // Do the call and return the JSON response
+        return $curl->post($url, $content);
     }
 
-    /**
-     * @return mixed
-     */
-    public function getToken()
+    public function getUser($id, array $fields = ['first_name', 'last_name'])
     {
-        return $this->token;
-    }
+        $curl = new Curl();
 
-    /**
-     * @param mixed $token
-     */
-    public function setToken($token)
-    {
-        $this->token = $token;
+        $url = self::FB_API_URL . '/' . $id;
+
+        $params = [
+            'fields' => implode($fields, ','),
+            'access_token' => $this->token
+        ];
+
+        return $curl->get($url, $params);
     }
 
 }
---
+++ src/JK/FacebookMessenger/Curl/Curl.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: john
+ * Date: 2016/06/20
+ * Time: 12:03 PM
+ */
+
+namespace JK\FacebookMessenger\Curl;
+
+/**
+ * Class Curl
+ * @package JK\FacebookMessenger\Curl
+ */
+class Curl
+{
+    /**
+     * @var resource
+     */
+    protected $ch;
+
+    /**
+     * @var string
+     */
+    protected $url;
+
+    /**
+     * @var array
+     */
+    protected $headers;
+
+    /**
+     * Curl constructor.
+     * @param null $url
+     * @param array $headers
+     */
+    public function __construct($url = null, $headers = [])
+    {
+        // Initialize some stuff
+        $this->ch = curl_init($url);
+        $this->headers = ['Content-Type: application/json'];
+
+        // Set default ops
+        curl_setopt($this->ch, CURLOPT_HTTPHEADER, $this->headers);
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->ch, CURLOPT_HEADER, 0);
+        curl_setopt($this->ch, CURLOPT_TIMEOUT, 30);
+    }
+
+    /**
+     * @param $url
+     * @param $content
+     * @return mixed
+     */
+    public function post($url, $content)
+    {
+        curl_setopt($this->ch, CURLOPT_URL, $url);
+        curl_setopt($this->ch, CURLOPT_POST, 1);
+        curl_setopt($this->ch, CURLOPT_POSTFIELDS, $content);
+
+        return curl_exec($this->ch);
+    }
+
+    /**
+     * @param $url
+     * @param array $params
+     * @return mixed
+     */
+    public function get($url, array $params)
+    {
+
+        $url .= '?' . http_build_query($params);
+
+        echo $url . PHP_EOL;
+
+        curl_setopt($this->ch, CURLOPT_URL, $url);
+
+        return curl_exec($this->ch);
+    }
+
+    /**
+     * Curl destructor
+     */
+    public function __destruct()
+    {
+        curl_close($this->ch);
+    }
+
+}
