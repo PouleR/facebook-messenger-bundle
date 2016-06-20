571ccbc29b48f8dcd8b2d734b2f494f503e8bab8
John <john@taxtim.com>
2016-06-20 13:00:44 +0200

    Added comments to MessengerAPI class, and updated methods to json_decode response.

--- src/JK/FacebookMessenger/Api/MessengerApi.php
+++ src/JK/FacebookMessenger/Api/MessengerApi.php
@@ -16,18 +16,38 @@ use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
+/**
+ * Class MessengerApi
+ * @package JK\FacebookMessenger\Api
+ */
 class MessengerApi
 {
     const FB_API_URL = 'https://graph.facebook.com/v2.6';
 
+    /**
+     * @var string
+     */
     protected $token;
 
+    /**
+     * @var array
+     */
     protected $encoders;
 
+    /**
+     * @var Serializer
+     */
     protected $serializer;
 
+    /**
+     * @var array
+     */
     protected $normalizers;
 
+    /**
+     * MessengerApi constructor.
+     * @param string $token
+     */
     public function __construct($token = '')
     {
         $this->encoders = [new JsonEncoder()];
@@ -37,6 +57,11 @@ class MessengerApi
         $this->token = $token;
     }
 
+    /**
+     * @param Recipient $recipient
+     * @param Message $message
+     * @return mixed
+     */
     public function sendMessage(Recipient $recipient, Message $message)
     {
 
@@ -54,21 +79,30 @@ class MessengerApi
             ], 'json');
 
         // Do the call and return the JSON response
-        return $curl->post($url, $content);
+        return json_decode($curl->post($url, $content), true);
     }
 
+    /**
+     * @param $id
+     * @param array $fields
+     * @return mixed
+     */
     public function getUser($id, array $fields = ['first_name', 'last_name'])
     {
+        // Init curl
         $curl = new Curl();
 
+        // Build the URL
         $url = self::FB_API_URL . '/' . $id;
 
+        // Add some params
         $params = [
             'fields' => implode($fields, ','),
             'access_token' => $this->token
         ];
 
-        return $curl->get($url, $params);
+        // Do the call and return the JSON response
+        return json_decode($curl->get($url, $params), true);
     }
 
 }
