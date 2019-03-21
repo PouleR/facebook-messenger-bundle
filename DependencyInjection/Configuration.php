51c743bd5cab3c97ce6682ae1ec13c85fcb79768
de Keijzer <martin@spinninrecords.nl>
2019-03-21 15:29:22 +0100

    Keep Symfony > 4.1 compatibility in tact

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -16,7 +16,12 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder('pouler_facebook_messenger');
-        $rootNode = $treeBuilder->getRootNode();
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('pouler_facebook_messenger');
+        }
 
         $rootNode
             ->children()
