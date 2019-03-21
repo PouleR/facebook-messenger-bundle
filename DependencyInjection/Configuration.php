c092287ee084c275b9d3aa32e83d4911ed9c3b15
de Keijzer <martin@spinninrecords.nl>
2019-03-21 14:34:49 +0100

    Fix #9 Add root node in Treebuilder config

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -15,8 +15,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('pouler_facebook_messenger');
+        $treeBuilder = new TreeBuilder('pouler_facebook_messenger');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
